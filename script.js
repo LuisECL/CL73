8465e99e7826c72db0a4190300e8251a3b923a35
Luis <lecarcamol@gmail.com>
2022-08-06 00:42:01 -0300

    feat: Adiciona mudança alternada de items no carrossel
    Criado objeto com info de cada item do carrossel nas linhas 4-34 de script.js, e função alternaCarrossel, e um setInterval() para excecutá-la a cada 5s

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-const navHamburguer = document.querySelector('.nav-hamburguer');
-const btnHamburguer = document.querySelector('.btn-hamburguer');
-const navHambClose = document.querySelector('.nav-hamb-close');
+const navHamburguer = document.querySelector(".nav-hamburguer");
+const btnHamburguer = document.querySelector(".btn-hamburguer");
+const navHambClose = document.querySelector(".nav-hamb-close");
+const carrosselItem = document.querySelector(".carrossel-item");
+const carrItemTextArray = document.querySelector(
+  ".carr-item-text-container"
+).children;
+const carrosselData = {
+  item01: {
+    bg: "01.colheita.jpg",
+    titulo: "Colheita",
+    subtitulo: "Do campo à sua casa",
+  },
+  item02: {
+    bg: "02.processamento.jpg",
+    titulo: "Separação",
+    subtitulo: "Os melhores grãos para o melhor café",
+  },
+  item03: {
+    bg: "03.industria.jpg",
+    titulo: "Fermentação",
+    subtitulo: "Tecnología em favor da qualidade",
+  },
+  item04: {
+    bg: "04.transporte.jpg",
+    titulo: "Transporte",
+    subtitulo: "Atingindo todos os cantos do Brasil",
+  },
+  item05: {
+    bg: "05.produto.jpg",
+    titulo: "Apresentação",
+    subtitulo: "Puro ou com leite, você que escolhe",
+  },
+};
 
 // --------------FUNÇÕES------------------
 function escondeNavHamb() {
   navHamburguer.style.animation = "nav-hamburguer-out .5s 1";
-  setTimeout(()=>{
+  setTimeout(() => {
     navHamburguer.style.display = "none";
   }, 500);
 }
@@ -15,12 +46,30 @@ function mostraNavHamb() {
   navHamburguer.style.animation = "nav-hamburguer-in .5s 1";
 }
 
+function alternaCarrossel(item) {
+  carrosselItem.style.backgroundImage = `url(img/carrossel/${item.bg})`;
+  carrItemTextArray[0].innerHTML = item.titulo;
+  carrItemTextArray[1].innerHTML = item.subtitulo;
+}
+
+alternaCarrossel(carrosselData.item01);
+let count = 2;
+const esperaParaAlternar = setInterval(() => {
+  if(count > 5){
+    count = 1
+  }
+  console.log(`Agora vai o item0${count}`);
+  let itemAtual = `item0${count}`
+  alternaCarrossel(carrosselData[itemAtual])
+  ++count
+}, 5000);
+// clearInterval(esperaParaAlternar);
 
 // --------------EVENTOS------------------
-btnHamburguer.addEventListener('click', ()=> {
-  mostraNavHamb()
-})
+btnHamburguer.addEventListener("click", () => {
+  mostraNavHamb();
+});
 
-navHambClose.addEventListener('click', ()=> {
-  escondeNavHamb()
-})
+navHambClose.addEventListener("click", () => {
+  escondeNavHamb();
+});
